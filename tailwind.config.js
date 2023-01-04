23dedf5ddebd1e926dc30b702b802729e16b1e7e
yatraavaghasia <113079616+yatraavaghasia@users.noreply.github.com>
2023-01-04 15:34:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary:{
+          100:"#E2E2D5",
+          200:"#888883"
+        }
+      }
+    },
+    fontFamily:{
+      body:['Nunito','sans-serif']
+    }
+  },
+  plugins: [],
+}
